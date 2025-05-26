b1b501b1f3518e2eaca65d8e3ecae4b5447f926a
Felipe Morais Gomez <anounymousfdp@gmail.com>
2025-05-25 23:15:12 -0300

    Create L2_A1.java

---
+++ Atividades JAVA/Lista de exercícios 02/L2_A1.java
@@ -0,0 +1,24 @@
+public class L2_A1 {
+	public static void main(String[] args) {
+			System.out.println("***");
+			System.out.println("***");
+			System.out.println("***\n");
+			
+			System.out.println("* ");
+			System.out.println("**");
+			System.out.println("***");
+			System.out.println("****\n");
+			
+			System.out.println("*****");
+			System.out.println("*");
+			System.out.println("***");
+			System.out.println("*");
+			System.out.println("*****\n");
+			
+			System.out.println("*****");
+			System.out.println("****");
+			System.out.println("***");
+			System.out.println("**");
+			System.out.println("*");
+	}
+}
