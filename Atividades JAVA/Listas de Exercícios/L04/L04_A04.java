44592f19e00b4eba78a52c23f978858c360200f7
Felipe Morais Gomez <anounymousfdp@gmail.com>
2025-06-17 11:01:54 -0300

    v

---
+++ Atividades JAVA/Listas de Exercícios/L04/L04_A04.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class L04_A04 {
+    public static void main(String[] args) {
+        Double A,B,C;
+
+        Scanner entrada = new Scanner(System.in);
+
+        A = entrada.nextDouble();
+        B = entrada.nextDouble().
+        C = entrada.nextDouble();
+
+        if (A == B && B == C) {
+            System.out.print("equilátero");
+        }
+        else if (A == B || B == C || C == A) {
+            System.out.print("isósceles");
+        }
+        else if (A != B && B != C && C != A) {
+            System.out.print("escaleno");
+        }
+
+
+    }
+}
