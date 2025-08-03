f5903a7c4f74da6a151999f35fa61c15cdf34790
Felipe Morais Gomez <anounymousfdp@gmail.com>
2025-08-03 17:20:23 -0300

    Update L06_A07.java

--- Atividades JAVA/Listas de Exercícios/L06/L06_A07.java
+++ Atividades JAVA/Listas de Exercícios/L06/L06_A07.java
@@ -1,10 +1,11 @@
 public class L06_A07 {
     public static void main(String[] args) {
-        int c;
-        c = 0;
+        int c, p;
+        c = 0; p = 1;
 
         while (c <= 15) {
-            System.out.printf("3^%d = %d%n",c,3*(3*c));
+            System.out.printf("3^%d = %d%n",c,p);
+            p = p * 3;
             c = ++c;
         }
     }
