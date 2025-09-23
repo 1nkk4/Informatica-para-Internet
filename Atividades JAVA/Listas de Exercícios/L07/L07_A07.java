bcfa19c9f642d9d96bf394ccc10404cda92463b4
Felipe Morais Gomez <anounymousfdp@gmail.com>
2025-09-23 01:35:39 -0300

    Create L07_A07.java

---
+++ Atividades JAVA/Listas de Exercícios/L07/L07_A07.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class L07_A07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//		Apresente os quadrados dos números inteiros de 15 a 200.
+        int quad;
+
+        for(int num = 15; num <= 200; num++) {
+            System.out.printf("%d²=%d%n",num,num*num);
+        }
+        
+    }
+}
