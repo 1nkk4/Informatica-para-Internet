eba575746f5ac85a83b4eb1b68e0bfa291cc9ff8
Felipe Morais Gomez <anounymousfdp@gmail.com>
2025-09-23 00:47:42 -0300

    Add input handling and summation logic

--- Atividades JAVA/Listas de Exercícios/L07/L7_A3.java
+++ Atividades JAVA/Listas de Exercícios/L07/L7_A3.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class L7_A3 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int v,t = 0;
+
+        for(int c = 1; c <= 10; c++ ) {
+            System.out.println("Digite o "+c+"° valor: ");
+            v = sc.nextInt();
+            t += v;
+        }
+
+        System.out.println("Resultado do somátorio: "+t);
+
     }
 }
