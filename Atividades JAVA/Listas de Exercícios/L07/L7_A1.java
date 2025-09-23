7da15047225c08d2804b734b97c4210773caccd3
Felipe Morais Gomez <anounymousfdp@gmail.com>
2025-09-23 00:24:37 -0300

    L07 wowie!!!!

---
+++ Atividades JAVA/Listas de Exercícios/L07/L7_A1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class L7_A1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int Dia, Mes, Ano; String nMes = null;
+
+        do {System.out.println("Digite o número do dia: "); Dia = sc.nextInt();
+            if(Dia < 1 || Dia > 31) {System.out.println("Valor inválido!");}}
+            while(Dia < 1 || Dia > 31);
+
+        do {System.out.println("Digite o número do mês: "); Mes = sc.nextInt();
+            if(Mes < 1 || Mes > 12) {System.out.println("Valor Inválido!");}}
+            while(Mes < 1 || Mes > 12);
+
+        switch(Mes) {
+            case 1: nMes = "Janeiro"; break;
+            case 2: nMes = "Fevereiro"; break;
+            case 3: nMes = "Março"; break;
+            case 4: nMes = "Abril"; break;
+            case 5: nMes = "Maio"; break;
+            case 6: nMes = "Junho"; break;
+            case 7: nMes = "Julho"; break;
+            case 8: nMes = "Agosto"; break;
+            case 9: nMes = "Setembro"; break;
+            case 10: nMes = "Outubro"; break;
+            case 11: nMes = "Novembro"; break;
+            case 12: nMes = "Dezembro"; break;
+        }
+
+        System.out.println("Digite o número do ano: ");
+        Ano = sc.nextInt();
+
+        System.out.printf("%d de %s de %d",Dia, nMes, Ano);
+
+    }
+}
---
+++ Atividades JAVA/Listas de Exercícios/L07/L7_A2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class L7_A2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int placa;
+
+        System.out.println("Digite o número da placa");
+        do {
+            placa = sc.nextInt();
+
+            if (placa > 9999 || placa < 1000) {
+                System.out.println("Valor inválido!");
+            }
+
+        } while(placa > 9999 || placa < 1000);
+
+        switch(placa % 10) {
+            case 0: System.out.println("deve se realizar a vistoria em Janeiro / 2014"); break;
+            case 1: System.out.println("deve se realizar a vistoria em Fevereiro / 2014"); break;
+            case 2: System.out.println("deve se realizar a vistoria em Março / 2014"); break;
+            case 3: System.out.println("deve se realizar a vistoria em Abril / 2014"); break;
+            case 4: System.out.println("deve se realizar a vistoria em Maio / 2014"); break;
+            case 5: System.out.println("deve se realizar a vistoria em Junho / 2014"); break;
+            case 6: System.out.println("deve se realizar a vistoria em Setembro deste ano"); break;
+            case 7: System.out.println("deve se realizar a vistoria em Outubro deste ano"); break;
+            case 8: System.out.println("deve se realizar a vistoria em Novembro deste ano"); break;
+            case 9: System.out.println("deve se realizar a vistoria em Dezembro deste ano"); break;
+        }
+
+    }
+}
