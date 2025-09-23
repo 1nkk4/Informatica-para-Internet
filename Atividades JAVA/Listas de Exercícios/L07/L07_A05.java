cc22ce1afcaef106accaa42fc519901598bf11b5
Felipe Morais Gomez <anounymousfdp@gmail.com>
2025-09-23 01:18:34 -0300

    Add L07_A05.java to calculate squares of integers

---
+++ Atividades JAVA/Listas de Exercícios/L07/L07_A05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class L07_A05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int quad, nume;
+        nume = 0;
+
+        do {
+            System.out.println("Digite um número inteiro (negativo para sair): ");
+            nume = sc.nextInt();
+            if(nume>=0) {
+                quad = nume*nume;
+                System.out.printf("%d²=%d%n",nume,quad);
+            }
+        } while(nume>=0);
+
+        System.out.println("Um valor negativo foi inserido, programa finalizado.");
+    }
+}
